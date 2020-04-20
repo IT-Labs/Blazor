1ab9044322e871ecdc41ca22393016931aa47edb
Aleksandar Gjoreski <aleksandar.gjoreski@it-labs.com>
2020-04-20 16:44:45 +0200

    create forcasts list component

---
+++ BlazorApp/BlazorApp.Client/Shared/ForecastsList.razor.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApp.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BlazorApp.Client.Shared
+{
+    public partial class ForecastsList
+    {
+        [Parameter] public List<WeatherForecast> Forecasts { get; set; }
+    }
+}
